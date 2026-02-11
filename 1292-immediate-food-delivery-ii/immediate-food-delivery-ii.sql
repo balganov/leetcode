137a752304fab8e3b56b63df515e0b56cfaa0e08
Nursultan <160471135+balganov@users.noreply.github.com>
2026-02-11 08:37:46 +0500

    Time: 421 ms (30.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+with cte as (
+    select *, 
+    (case when order_date=customer_pref_delivery_date then 1 else 0 end) as order_type,
+    row_number() over(partition by customer_id order by order_date) as order
+    from delivery
+)
+select round(sum(order_type)*100.0/count(*),2) as immediate_percentage 
+from cte
+where "order" = 1
