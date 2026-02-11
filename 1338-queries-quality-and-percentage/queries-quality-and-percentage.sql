fa2f9ea3f2a6049e4e30565432efb38ccec399c9
Nursultan <160471135+balganov@users.noreply.github.com>
2026-02-11 07:43:46 +0500

    Time: 259 ms (19.61%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 select query_name,
         round(avg(rating::decimal/position),2) as quality,
-        round((count(rating) filter (where rating<3)*100.0)/count(*),2) as poor_query_percentage
+        round(avg((rating<3)::int)*100,2) as poor_query_percentage
 from queries
 group by query_name
